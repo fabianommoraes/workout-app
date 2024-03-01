ee9585a0f7e57b722626915daa7095cb371f6766
fabianommoraes <fabiano.m.moraes@gmail.com>
2024-03-01 14:09:46 -0300

    icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="description" content="" />
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png"></link>
           <meta name="theme-color" content="#fff" />
